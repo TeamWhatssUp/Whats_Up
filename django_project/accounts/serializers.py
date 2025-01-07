f6d5afb149e77ddd48296ada55b5d3b8eeb9bfaa
KimDongChna95 <impact7608@gmail.com>
2025-01-07 17:59:16 +0900

    WU-17 update:회원가입 api 테스트 완

--- django_project/accounts/serializers.py
+++ django_project/accounts/serializers.py
@@ -1,19 +1,24 @@
 from rest_framework import serializers
 from .models import CustomUser
+from django.db import IntegrityError
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+
     class Meta:
         model = CustomUser
         fields = ['username', 'password', 'email']
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
-            username=validated_data['username'],
-            password=validated_data['password'],
-            email=validated_data['email']
-        )
-        return user
+        try:
+            user = CustomUser.objects.create_user(
+                username=validated_data['username'],
+                password=validated_data['password'],
+                email=validated_data['email']
+            )
+            return user
+        except IntegrityError:
+            raise serializers.ValidationError({'error': 'Username or email already exists.'})
 
 class InitialSetupSerializer(serializers.ModelSerializer):
     class Meta:
