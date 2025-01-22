afbbafc513b40d1c803dd28935df82d7b88736fe
KimDongChna95 <impact7608@gmail.com>
2025-01-22 11:49:22 +0900

    Wu-17 fis:회원가입 후 로그인 문제 해결2

--- django_project/accounts/serializers.py
+++ django_project/accounts/serializers.py
@@ -6,3 +6,10 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email')
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
