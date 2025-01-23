1832fe876db7a63232ad77dbed1a1babdf1655f3
jackson8930 <wotjr7012@gmail.com>
2025-01-23 20:21:56 +0900

    commit

--- django_project/accounts/admin.py
+++ django_project/accounts/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, Conversation, ChatMessage
 
+# CustomUser 모델 등록
 admin.site.register(CustomUser, UserAdmin)
+
+# Conversation 모델 등록
+admin.site.register(Conversation)
+
+# ChatMessage 모델 등록
+admin.site.register(ChatMessage)
