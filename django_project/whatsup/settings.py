39a55e233452d832647a9275f6288b072f4ecba6
Alex-Poatan-Pereira <hwaeeting0104@gmail.com>
2025-01-30 11:36:58 +0900

    middleware

--- django_project/whatsup/settings.py
+++ django_project/whatsup/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'whatsup.middleware.HealthCheckMiddleware',
-    "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
